c3e7a497bea09c95f81a8d686c12c0255b300258
yogesh1234567890 <yogeshbhattarai073gmail.com>
2021-09-07 21:19:24 +0545

    managing static files and dirs

--- smartbaje/urls.py
+++ smartbaje/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('', include('viewfront.urls')),
-    path('auth/', include('account.urls')),
+    path('auth/', include('accounts.urls')),
 ]
--- viewfront/views.py
+++ viewfront/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def viewpage(request):
+    return render(request,"viewfront/index-4.html")
