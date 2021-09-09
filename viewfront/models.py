3f9be6eed5e1f91a7ee3fbc0d7f8f2cbdf634f0a
yogesh1234567890 <yogeshbhattarai073gmail.com>
2021-09-09 22:56:51 +0545

    created product and category models and implemented some loops

--- viewfront/models.py
+++ viewfront/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural="Categories"
+
+    def __str__(self):
+        return self.name
+
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=500,null=True,blank=True)
+    image = models.ImageField(upload_to="categories",null=True,blank=True)
+    slug=models.SlugField(max_length=100, unique=True)
+
+
+
+
+class Product(models.Model):
+
+    class Meta:
+        verbose_name_plural="Products"
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.SlugField()
+    description = models.TextField(max_length=500,null=True,blank=True)
+    image = models.ImageField(upload_to="products")
+    ratings = models.FloatField(null=True,blank=True)
+
