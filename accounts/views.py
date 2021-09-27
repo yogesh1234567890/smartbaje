ee25858831dc9aedb083acad7a04cc9067013775
yogesh1234567890 <yogeshbhattarai073gmail.com>
2021-09-27 11:56:24 +0545

    login and register

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,128 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import Account
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+# Verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+from cart.views import _cart_id
+from cart.models import Cart, CartItem
+import requests
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST' and request.is_ajax:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = first_name+" "+last_name
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+
+            # USER ACTIVATION
+            # current_site = get_current_site(request)
+            # mail_subject = 'Please activate your account'
+            # message = render_to_string('accounts/account_verification_email.html', {
+            #     'user': user,
+            #     'domain': current_site,
+            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            #     'token': default_token_generator.make_token(user),
+            # })
+            # to_email = email
+            # send_email = EmailMessage(mail_subject, message, to=[to_email])
+            # send_email.send()
+            messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address [rathan.kumar@gmail.com]. Please verify it.')
+            return redirect('auth:register')
+            # return redirect('/accounts/login/?command=verification&email='+email)
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'register.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            # try:
+            #     cart = Cart.objects.get(cart_id=_cart_id(request))
+            #     is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+            #     if is_cart_item_exists:
+            #         cart_item = CartItem.objects.filter(cart=cart)
+
+                    # Getting the product variations by cart id
+                    # product_variation = []
+                    # for item in cart_item:
+                    #     variation = item.variations.all()
+                    #     product_variation.append(list(variation))
+
+                    # Get the cart items from the user to access his product variations
+                    # cart_item = CartItem.objects.filter(user=user)
+                    # ex_var_list = []
+                    # id = []
+                    # for item in cart_item:
+                    #     existing_variation = item.variations.all()
+                    #     ex_var_list.append(list(existing_variation))
+                    #     id.append(item.id)
+
+                    # product_variation = [1, 2, 3, 4, 6]
+                    # ex_var_list = [4, 6, 3, 5]
+
+            #         for pr in product_variation:
+            #             if pr in ex_var_list:
+            #                 index = ex_var_list.index(pr)
+            #                 item_id = id[index]
+            #                 item = CartItem.objects.get(id=item_id)
+            #                 item.quantity += 1
+            #                 item.user = user
+            #                 item.save()
+            #             else:
+            #                 cart_item = CartItem.objects.filter(cart=cart)
+            #                 for item in cart_item:
+            #                     item.user = user
+            #                     item.save()
+            # except:
+            #     pass
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            url = request.META.get('HTTP_REFERER')
+            # try:
+            #     query = requests.utils.urlparse(url).query
+                # next=/cart/checkout/
+            #     params = dict(x.split('=') for x in query.split('&'))
+            #     if 'next' in params:
+            #         nextPage = params['next']
+            #         return redirect(nextPage)                
+            # except:
+            #     return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+    return render(request, 'login.html')
+
+
+@login_required(login_url = 'login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect('auth:login')
+
